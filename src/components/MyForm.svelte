5fa8118cd3a3ffc2a130ecef9d9794c614b0ba03
Doug <doughill1000@gmail.com>
2021-02-03 20:03:02 -0500

    Grouping

--- src/components/MyForm.svelte
+++ src/components/MyForm.svelte
@@ -4,12 +4,18 @@
     const { firstname, lastname } = myFormStore;
 </script>
 
-<form autocomplete="off">
-    <label class="label mt-4 is-inline-flex" for="last-name">
-        First Name
-    </label>
-    <input id="last-name" bind:value={$firstname} />
+<div>
+    <form autocomplete="off">
+        <div>
+            <label class="label mt-4 is-inline-flex" for="last-name">
+                First Name
+            </label>
 
-    <label class="label" for="first-name"> Last Name </label>
-    <input id="first-name" bind:value={$lastname} />
-</form>
+            <input id="last-name" bind:value={$firstname} />
+        </div>
+        <div>
+            <label class="label" for="first-name"> Last Name </label>
+            <input id="first-name" bind:value={$lastname} />
+        </div>
+    </form>
+</div>
